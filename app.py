2e100326dbc2ffd1e4ff3de86a63b37ef0c16eb2
kyle01 <kylemcv@gmail.com>
2024-02-19 09:26:00 -0500

    Commit fetch

--- app.py
+++ app.py
@@ -1,5 +1,16 @@
+import os
 from flask import Flask, jsonify
+import psycopg2
+from dotenv import load_dotenv
+
 app = Flask(__name__)
+
+load_dotenv() 
+
+def get_db_connection():
+    DB_CONNECTION_URL = os.environ.get('DB_CONNECTION_URL')
+    conn = psycopg2.connect(DB_CONNECTION_URL)
+    return conn
 
 @app.route('/')
 def hello_world():
@@ -10,3 +21,14 @@ def second_endpoint():
     return jsonify(
         foo="bar",
     )
+
+
+@app.route('/get_books')
+def index():
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute('SELECT * FROM books;')
+    books = cur.fetchall()
+    cur.close()
+    conn.close()
+    return books
